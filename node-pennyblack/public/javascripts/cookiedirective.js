1811f3cc15c2c601cf2fdb86a9c4b3e1769b5095
Timothy <timothy.rochford@gmail.com>
2016-01-10 12:41:45 +0200

    node-pennyblack: EU cookie directive font colour changes

--- node-pennyblack/public/javascripts/cookiedirective.js
+++ node-pennyblack/public/javascripts/cookiedirective.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     $.cookiesDirective({
                            explicitConsent: false,
                            position: 'bottom',
-        privacyPolicyUri: 'privacy-policy'
-
+        privacyPolicyUri: 'privacy-policy',
+        fontColor: '#CA0000',
+        backgroundColor: '#000000',
+        backgroundOpacity: '99', // opacity of disclosure panel
     });
 });
